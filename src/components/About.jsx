d264aae9657c8f6b468a2cb9f8e71ec1dcb61358
nitesh-20 <nsahu6156@gmail.com>
2024-05-30 00:12:19 +0530

    About fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ import { ABOUT_TEXT } from '../constants'
 const About = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h1 className='my-20 text-center text-4xl'>About 
-        <span className='text-neutral-500'>Me</span>
+        <h1 className='my-20 text-center text-4xl'>About
+        <span className='text-neutral-500'> Me</span>
         </h1>
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2 lg:p-8'>
@@ -16,7 +16,7 @@ const About = () => {
             </div>
                 <div className='w-full lg:w-1/2'>
                     <div className='flex justify-center lg:justify-start'>
-                        <p className=''>{ABOUT_TEXT}</p>
+                        <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
                </div>
              </div>
         </div>
