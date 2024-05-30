9d4926742f71c365bcd1c21dc5b66ebcc8c1546d
nitesh-20 <nsahu6156@gmail.com>
2024-05-30 16:40:18 +0530

    Technologies and Experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Technologies from './components/Technologies'
+import Experience from './components/Experience'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
       <Navbar />
       <Hero/>
       <About/>
+      <Technologies/>
+      <Experience/>
      </div>
     </div>
   )
